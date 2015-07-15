a31e8cf19b670ddc0183795822b7b9056924d778
max <m@xk.io>
2015-07-15 17:46:43 +1000

    Continue towards MVP

--- BlocVoting/Instructions/Empower.hs
+++ BlocVoting/Instructions/Empower.hs
@@ -10,11 +10,18 @@ import BlocVoting.Binary (get4ByteInt)
 data OpEmpower = OpEmpower {
     empVotes :: Int
   , empAddress :: BS.ByteString
+  , empND :: ND.Nulldata
 }
   deriving (Show, Eq)
 
+_isValidNulldata :: ND.Nulldata -> Bool
+_isValidNulldata nd@(ND.Nulldata msg sender)
+	| BS.length msg /= 30 = False
+	| decodeBase58Check (encodeBase58 addrToEmpower) == Nothing = False
+	| otherwise = True
+	where addrToEmpower = BS.drop 5 msg
 
-fromNulldata :: ND.Nulldata -> OpEmpower
-fromNulldata (ND.Nulldata bytes senderAddress) = OpEmpower votes addressPretty
-  where votes = get4ByteInt (BS.drop 1 bytes) 0
-        addressPretty = encodeBase58 $ BS.drop 5 bytes
+fromNulldata :: ND.Nulldata -> Maybe OpEmpower
+fromNulldata nd@(ND.Nulldata msg senderAddress) = if _isValidNulldata nd then Just $ OpEmpower votes addressPretty else Nothing
+  where votes = get4ByteInt (BS.drop 1 msg) 0
+        addressPretty = encodeBase58 $ BS.drop 5 msg
